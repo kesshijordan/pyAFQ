679858ef7bf570f2dc8a109ab8f3c18cfc2c2391
kesshijordan <Kesshi.Jordan@ucsf.edu>
2016-09-07 01:33:48 -0700

    Fixed bug in fetching fetcher

--- examples/plot_tract_profile.py
+++ examples/plot_tract_profile.py
@@ -28,7 +28,7 @@ import AFQ.segmentation as seg
 dpd.fetch_stanford_hardi()
 dpd.fetch_stanford_t1()
 
-hardi_dir = op.join(fetcher.dipy_home, "stanford_hardi")
+hardi_dir = op.join(dpd.fetcher.dipy_home, "stanford_hardi")
 hardi_fdata = op.join(hardi_dir, "HARDI150.nii.gz")
 hardi_fbval = op.join(hardi_dir, "HARDI150.bval")
 hardi_fbvec = op.join(hardi_dir, "HARDI150.bvec")
